e1894c1d3f589435c8c6f6929b789c746fc7f805
gongpei <ggongpei@gmail.com>
2022-05-07 23:37:06 +0800

    feat: page default code

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,19 +1,5 @@
 import Layout from '../../components/layout';
 
-export default function Post({ postData }: {
-  postData: {
-    id: string;
-    date: string;
-    title: string
-  }
-}) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
-  );
+export default function Post() {
+  return <div></div>;
 }
